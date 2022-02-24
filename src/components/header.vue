fcc7a2e54f305fb0b5ac1b53d34c4cbf29a1412e
Jesse <akohgodwinjesse@gmail.com>
2022-02-24 18:33:47 +0100

    whodey

---
+++ src/components/header.vue
@@ -0,0 +1,30 @@
+<template>
+   <header class="child_wrap flex">
+        <div class="logo">
+           <img src="@/assets/logo.svg" alt="">
+        </div>
+        <nav>
+            <ul class="flex">
+                <li>Testimonials</li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li>Login</li>
+            </ul>
+        </nav>
+        <div class="btn login">
+            <button>Sign up</button>
+        </div>
+        
+   </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss" src="../scss/header.scss">
+
+</style>
