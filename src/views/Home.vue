3421eb99f41041b54cc79726a5bcfd3e529724f3
Jesse <akohgodwinjesse@gmail.com>
2022-02-24 19:25:32 +0100

    whodey

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,15 @@
   <div class="home">
      <Header/>
      <main class="child_wrap flex">
-       <div class="txt_copy">
-         <h1>Who Dey Breathe??</h1>
-         <p>Date a Cultist Near You today</p>
+       <div class="txt_copy flex flex_col">
+         <h1>Date a Cultist Near You today</h1>
          <p>By clicking Join, you agree to our Terms. Learn how we process your data in our Privacy Policy and Cookies Policy.</p>
          <div class="btn">
            <button>Join Our Waitlist</button>
          </div>
        </div>
        <div class="hero_img img">
-         <img src="@/assets/axe.png" alt="">
+         <img src="@/assets/vegan.png" alt="">
        </div>
      </main>
   </div>
